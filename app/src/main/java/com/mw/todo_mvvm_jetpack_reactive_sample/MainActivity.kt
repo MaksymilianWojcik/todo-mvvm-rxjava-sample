23c4936506289798f0a55b0fd00a9d56a5c8f060
Maksymilian Wojcik <maksymilian.wojcik@intive.com>
2020-12-11 11:38:58 +0100

    Init empty project

---
+++ app/src/main/java/com/mw/todo_mvvm_jetpack_reactive_sample/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mw.todo_mvvm_jetpack_reactive_sample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}

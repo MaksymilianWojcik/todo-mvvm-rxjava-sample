273b626ab03011ceb3f29fe08cd65956ac1ad336
Maksymilian Wojcik <maksymilian.wojcik@intive.com>
2020-12-11 13:20:15 +0100

    Added bottom navigation with separate graph for each item

--- app/src/main/java/com/mw/todo_mvvm_jetpack_reactive_sample/ui/activity/MainActivity.kt
+++ app/src/main/java/com/mw/todo_mvvm_jetpack_reactive_sample/ui/activity/MainActivity.kt
@@ -2,11 +2,57 @@ package com.mw.todo_mvvm_jetpack_reactive_sample.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.observe
+import androidx.navigation.NavController
+import androidx.navigation.NavDestination
 import com.mw.todo_mvvm_jetpack_reactive_sample.R
+import com.mw.todo_mvvm_jetpack_reactive_sample.databinding.ActivityMainBinding
+import com.mw.todo_mvvm_jetpack_reactive_sample.utils.setupWithNavController
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class MainActivity : AppCompatActivity(), NavController.OnDestinationChangedListener {
+
+    private var currentNavController: LiveData<NavController>? = null
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupBottomNavigation()
+    }
+
+    override fun onDestinationChanged(
+        controller: NavController,
+        destination: NavDestination,
+        arguments: Bundle?
+    ) {
+        Timber.d("Destination changed: ${controller.graph}, dest: $destination")
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return currentNavController?.value?.navigateUp() ?: super.onSupportNavigateUp()
+    }
+
+    private fun setupBottomNavigation() {
+        val navGraphIds = listOf(
+            R.navigation.tasks_graph,
+            R.navigation.settings_graph
+        )
+        val controller = binding.bottomNav.setupWithNavController(
+            navGraphIds = navGraphIds,
+            fragmentManager = supportFragmentManager,
+            containerId = R.id.nav_host_fragment,
+            intent = intent
+        )
+        controller.observe(this) { navController ->
+            // TODO: Add custom toolbar
+            currentNavController?.value?.removeOnDestinationChangedListener(this)
+            currentNavController = controller
+            currentNavController?.value?.addOnDestinationChangedListener(this)
+        }
     }
 }

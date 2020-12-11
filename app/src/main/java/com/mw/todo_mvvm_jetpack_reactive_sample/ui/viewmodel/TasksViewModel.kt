a36a8baf81d7668278187a40d581a37aa5cdd2de
Maksymilian Wojcik <maksymilian.wojcik@intive.com>
2020-12-11 16:13:02 +0100

    Observing tasks in realtime in TasksViewModel

--- app/src/main/java/com/mw/todo_mvvm_jetpack_reactive_sample/ui/viewmodel/TasksViewModel.kt
+++ app/src/main/java/com/mw/todo_mvvm_jetpack_reactive_sample/ui/viewmodel/TasksViewModel.kt
@@ -2,15 +2,46 @@ package com.mw.todo_mvvm_jetpack_reactive_sample.ui.viewmodel
 
 import androidx.hilt.lifecycle.ViewModelInject
 import androidx.lifecycle.ViewModel
+import com.mw.todo_mvvm_jetpack_reactive_sample.domain.usecase.GetTasksUseCase
 import com.mw.todo_mvvm_jetpack_reactive_sample.ui.model.TasksNavigationDestination
 import com.mw.todo_mvvm_jetpack_reactive_sample.utils.SingleLiveEvent
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import io.reactivex.schedulers.Schedulers
+import timber.log.Timber
 
-class TasksViewModel @ViewModelInject constructor() : ViewModel() {
+class TasksViewModel @ViewModelInject constructor(
+    private val getTasksUseCase: GetTasksUseCase
+) : ViewModel() {
 
     private val _navigationDestination = SingleLiveEvent<TasksNavigationDestination>()
     val navigationDestination = _navigationDestination
 
+    private val compositeDisposable = CompositeDisposable()
+
+    init {
+        observeTasks()
+    }
+
     fun addNewTask() {
         navigationDestination.value = TasksNavigationDestination.NewTask
+    }
+
+    private fun observeTasks() {
+        compositeDisposable.add(
+            getTasksUseCase.observeTasks()
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe({
+                    Timber.d("Observing tasks: $it")
+                }, {
+                    Timber.e("Error observing tasks: $it")
+                })
+        )
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposable.clear()
     }
 }

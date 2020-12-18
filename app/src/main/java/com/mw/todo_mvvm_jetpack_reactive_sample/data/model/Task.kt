882b1ff935f487acc5da565beb533e0f22f78685
Maksymilian Wojcik <maksymilian.wojcik@intive.com>
2020-12-18 10:23:49 +0100

    Removed unnecessary constructor for kotlin Task object as default arguments values are provided

--- app/src/main/java/com/mw/todo_mvvm_jetpack_reactive_sample/data/model/Task.kt
+++ app/src/main/java/com/mw/todo_mvvm_jetpack_reactive_sample/data/model/Task.kt
@@ -1,10 +1,8 @@
 package com.mw.todo_mvvm_jetpack_reactive_sample.data.model
 
 data class Task(
-    var id: String? = null,
+    var id: String? = "",
     var title: String = "",
     var description: String = "",
     @field:JvmField var isCompleted: Boolean = false
-) {
-    constructor() : this(null)
-}
+)
